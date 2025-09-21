0eb4369ebef6ed29122e3af153330091619f053e
a3devs-company <abhisaxena215@gmail.com>
2025-09-22 00:20:21 +0530

    fixed overflow error

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import { gsap } from 'gsap';
-
-import Btn from './button';
 import { useState } from 'react';
 const Navbar = () => {
   const logoRef = useRef(null);
@@ -45,7 +43,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className='w-screen '>
+    <div className='w-full '>
     <nav className="w-full h-[10vh] bg-white text-black  flex items-center justify-between px-10 py-4">
       <div className="logo w-1/3 h-full flex items-center">
         <div ref={logoRef} className="image w-25 ">
@@ -83,17 +81,21 @@ const Navbar = () => {
         </Link>
       </div>
       <div className="contact  w-1/3 h-full flex items-center justify-end">
-   {/* <Link to="/contactus " className='hover:scale-105 transition-all duration-75 hover:-translate-y-1'>
+   <Link to="/contactus " className='hidden md:block hover:scale-105 transition-all duration-75 hover:-translate-y-1'>
         <button
           ref={buttonRef}
           className="px-3 py-2 cursor-pointer  bg-[#FFCF14] rounded-lg"
         >
           Contact
         </button>
-   </Link> */}
-    <Btn className="md:hidden" ref={buttonRef} name="Contact US" link="/contactus">
-
-    </Btn>
+   </Link>
+    <div className="md:hidden">
+          <button className="text-black hover:text-blue-300 focus:outline-none" onClick={handleBurgerClick}>
+            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+          </button>
+        </div>
       </div>
     </nav>
       <div className={`absolute left-0 w-full   h-full inset-0  bg-black/40 backdrop-blur-sm bg-opacity-80 z-50 flex flex-col items-center justify-center transition-transform duration-300 ${showMobileMenu ? 'translate-x-0' : '-translate-x-full'} md:hidden`} style={{ pointerEvents: showMobileMenu ? 'auto' : 'none' }}>
